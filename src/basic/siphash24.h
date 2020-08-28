32926cc2c6b4512a36876f1c8130ebca7f8849d0
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2020-08-28 12:21:48 +0200

    Rework how we cache mtime to figure out if units changed
    Instead of assuming that more-recently modified directories have higher mtime,
    just look for any mtime changes, up or down. Since we don't want to remember
    individual mtimes, hash them to obtain a single value.
    
    This should help us behave properly in the case when the time jumps backwards
    during boot: various files might have mtimes that in the future, but we won't
    care. This fixes the following scenario:
    
    We have /etc/systemd/system with T1. T1 is initially far in the past.
    We have /run/systemd/generator with time T2.
    The time is adjusted backwards, so T2 will be always in the future for a while.
    Now the user writes new files to /etc/systemd/system, and T1 is updated to T1'.
    Nevertheless, T1 < T1' << T2.
    We would consider our cache to be up-to-date, falsely.
    
    (cherry picked from commit c2911d48ff0fc61fb3cfab7050110992a7390417)

--- src/basic/siphash24.h
+++ src/basic/siphash24.h
@@ -6,6 +6,8 @@
 #include <string.h>
 #include <sys/types.h>
 
+#include "time-util.h"
+
 struct siphash {
         uint64_t v0;
         uint64_t v1;
@@ -19,6 +21,10 @@ void siphash24_init(struct siphash *state, const uint8_t k[static 16]);
 void siphash24_compress(const void *in, size_t inlen, struct siphash *state);
 void siphash24_compress_boolean(bool in, struct siphash *state);
 #define siphash24_compress_byte(byte, state) siphash24_compress((const uint8_t[]) { (byte) }, 1, (state))
+
+static inline void siphash24_compress_usec_t(usec_t in, struct siphash *state) {
+        siphash24_compress(&in, sizeof in, state);
+}
 
 uint64_t siphash24_finalize(struct siphash *state);
 
