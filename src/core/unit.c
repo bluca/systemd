e0c2eee7beb07bc04075a4d7211b69d14741205d
Maanya Goenka <t-magoenka@microsoft.com>
2021-07-07 18:28:20 -0700

    manager: add a test flag to ignore dependencies
    The MANAGER_TEST_RUN_IGNORE_DEPENDENCIES flag was added in order to allow the caller
    to skip the recursive loading of dependency units when loading specific
    unit files. This includes the default dependencies, the specified dependencies, the slice.
    This will be used by systemd-analyze to allow checking individual unit files in isolation.
    
    (cherry picked from commit f14d6810e0a08a1ed5e9f1a213d71f9a531ccf50)

--- src/core/unit.c
+++ src/core/unit.c
@@ -3085,6 +3085,9 @@ int unit_add_dependency(
                 return 0;
         }
 
+        if (u->manager && FLAGS_SET(u->manager->test_run_flags, MANAGER_TEST_RUN_IGNORE_DEPENDENCIES))
+                return 0;
+
         /* Note that ordering a device unit after a unit is permitted since it
          * allows to start its job running timeout at a specific time. */
         if (d == UNIT_BEFORE && other->type == UNIT_DEVICE) {
@@ -3193,6 +3196,9 @@ int unit_add_dependency_by_name(Unit *u, UnitDependency d, const char *name, boo
         if (r < 0)
                 return r;
 
+        if (u->manager && FLAGS_SET(u->manager->test_run_flags, MANAGER_TEST_RUN_IGNORE_DEPENDENCIES))
+                return 0;
+
         r = manager_load_unit(u->manager, name, NULL, NULL, &other);
         if (r < 0)
                 return r;
@@ -3211,6 +3217,9 @@ int unit_add_two_dependencies_by_name(Unit *u, UnitDependency d, UnitDependency 
         r = resolve_template(u, name, &buf, &name);
         if (r < 0)
                 return r;
+
+        if (u->manager && FLAGS_SET(u->manager->test_run_flags, MANAGER_TEST_RUN_IGNORE_DEPENDENCIES))
+                return 0;
 
         r = manager_load_unit(u->manager, name, NULL, NULL, &other);
         if (r < 0)
@@ -3325,6 +3334,9 @@ int unit_set_default_slice(Unit *u) {
         int r;
 
         assert(u);
+
+        if (u->manager && FLAGS_SET(u->manager->test_run_flags, MANAGER_TEST_RUN_IGNORE_DEPENDENCIES))
+                return 0;
 
         if (UNIT_ISSET(u->slice))
                 return 0;
