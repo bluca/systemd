93c69872acb27d09ab7075b5745f656baaaddd67
Lennart Poettering <lennart@poettering.net>
2023-02-23 10:25:27 +0100

    importd: hook up memory pressure, SIGRTMIN-18 (and in fact SIGINT + SIGTERM)
    (cherry picked from commit 82fa9f28f9aba7d5dc47ab99fb148688ffae78f7)

--- src/import/importd.c
+++ src/import/importd.c
@@ -11,6 +11,7 @@
 #include "bus-log-control-api.h"
 #include "bus-polkit.h"
 #include "def.h"
+#include "common-signal.h"
 #include "env-util.h"
 #include "fd-util.h"
 #include "float.h"
@@ -645,7 +646,23 @@ static int manager_new(Manager **ret) {
         if (r < 0)
                 return r;
 
-        sd_event_set_watchdog(m->event, true);
+        (void) sd_event_set_watchdog(m->event, true);
+
+        r = sd_event_add_signal(m->event, NULL, SIGINT, NULL, NULL);
+        if (r < 0)
+                return r;
+
+        r = sd_event_add_signal(m->event, NULL, SIGTERM, NULL, NULL);
+        if (r < 0)
+                return r;
+
+        r = sd_event_add_signal(m->event, NULL, SIGRTMIN+18, sigrtmin18_handler, NULL);
+        if (r < 0)
+                return r;
+
+        r = sd_event_add_memory_pressure(m->event, NULL, NULL, NULL);
+        if (r < 0)
+                log_debug_errno(r, "Failed allocate memory pressure event source, ignoring: %m");
 
         r = sd_bus_default_system(&m->bus);
         if (r < 0)
@@ -1398,7 +1415,7 @@ static int run(int argc, char *argv[]) {
 
         umask(0022);
 
-        assert_se(sigprocmask_many(SIG_BLOCK, NULL, SIGCHLD, -1) >= 0);
+        assert_se(sigprocmask_many(SIG_BLOCK, NULL, SIGCHLD, SIGTERM, SIGINT, SIGRTMIN+18, -1) >= 0);
 
         r = manager_new(&m);
         if (r < 0)
