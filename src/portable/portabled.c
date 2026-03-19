bfc5c4da575cd7010037d47ab4a54f5bff0d273e
Luca Boccassi <luca.boccassi@gmail.com>
2026-03-19 11:19:55 +0000

    wip

--- src/portable/portabled.c
+++ src/portable/portabled.c
@@ -181,3 +181,4 @@ static int run(int argc, char *argv[]) {
 }
 
 DEFINE_MAIN_FUNCTION(run);
+//foo
--- src/resolve/resolved-llmnr.h
+++ src/resolve/resolved-llmnr.h
@@ -13,3 +13,4 @@ int manager_llmnr_ipv6_tcp_fd(Manager *m);
 void manager_llmnr_stop(Manager *m);
 void manager_llmnr_maybe_stop(Manager *m);
 int manager_llmnr_start(Manager *m);
+//foo
